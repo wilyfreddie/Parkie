a03a38766cbf15564e628be236ec5239f23ad56f
Ivan <garanivan59@gmail.com>
2021-03-06 18:28:51 +0800

    Removed unnecessary libraries

--- src/main/java/com/wilyfreddie/Parkie/Repositories/VehicleHistoryRepository.java
+++ src/main/java/com/wilyfreddie/Parkie/Repositories/VehicleHistoryRepository.java
@@ -1,10 +1,8 @@
 package com.wilyfreddie.Parkie.Repositories;
 
-import com.wilyfreddie.Parkie.Model.Vehicle;
 import com.wilyfreddie.Parkie.Model.VehicleHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface VehicleHistoryRepository extends JpaRepository<VehicleHistory,Integer> {
 
